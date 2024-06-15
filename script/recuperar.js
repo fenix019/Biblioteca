50a564681d939e1d83f34daa830334d99a997b18
kevin <kevinlarioshernandez@gmail.com>
2024-06-15 02:59:10 -0600

    commit

--- script/recuperar.js
+++ script/recuperar.js
@@ -15,6 +15,10 @@ document.addEventListener('DOMContentLoaded', function() {
     emailInput.addEventListener('input', function(event) {
         if (emailInput.validity.patternMismatch) {
             emailInput.setCustomValidity('Ingrese un correo válido de Gmail, Outlook o Hotmail');
+        } else if (emailInput.validity.valueMissing) {
+            emailInput.setCustomValidity('Este campo es requerido');
+        } else if (emailInput.value.indexOf('@') < 4) {
+            emailInput.setCustomValidity('El correo debe contener al menos 4 caracteres antes del @');
         } else {
             emailInput.setCustomValidity('');
         }
@@ -26,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             emailInput.classList.remove('is-valid');
             emailInput.classList.add('is-invalid');
-            emailInput.nextElementSibling.textContent = 'Por favor ingrese un correo válido (Gmail, Outlook o Hotmail).';
+            emailInput.nextElementSibling.textContent = emailInput.validationMessage;
         }
     });
 
@@ -48,3 +52,4 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+/** aaaaaaaaaaaaaaaaaaaaaaaaa ya nms ya quedo que ia no le muevan a esta webada :'3**/
