7d88e457189a0e98547fca1394433f4b0aa29ad0
kevin <kevinlarioshernandez@gmail.com>
2024-06-15 02:43:38 -0600

    commit

--- script/recuperar.js
+++ script/recuperar.js
@@ -3,6 +3,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const emailInput = form.querySelector('#inputEmail');
     const passwordInput = form.querySelector('#inputPassword');
 
+    // Función para validar el campo de correo electrónico en tiempo real
+    emailInput.addEventListener('input', function(event) {
+        if (emailInput.validity.valueMissing) {
+            emailInput.setCustomValidity('Este campo es requerido');
+        } else if (emailInput.validity.patternMismatch) {
+            emailInput.setCustomValidity('Ingrese un correo válido de Gmail, Outlook o Hotmail');
+        } else {
+            emailInput.setCustomValidity('');
+        }
+    });
+
+    // Función para validar el campo de contraseña en tiempo real
+    passwordInput.addEventListener('input', function(event) {
+        if (passwordInput.validity.valueMissing) {
+            passwordInput.setCustomValidity('Este campo es requerido');
+        } else if (passwordInput.validity.patternMismatch) {
+            passwordInput.setCustomValidity('La contraseña no debe contener espacios');
+        } else {
+            passwordInput.setCustomValidity('');
+        }
+    });
+
+    // Evitar que el formulario se valide automáticamente antes de enviarlo
     form.addEventListener('submit', function(event) {
         if (!form.checkValidity()) {
             event.preventDefault();
@@ -10,21 +33,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         form.classList.add('was-validated');
-    });
-
-    emailInput.addEventListener('input', function(event) {
-        if (emailInput.validity.patternMismatch) {
-            emailInput.setCustomValidity('Ingrese un correo válido de Gmail, Outlook o Hotmail');
-        } else {
-            emailInput.setCustomValidity('');
-        }
-    });
-
-    passwordInput.addEventListener('input', function(event) {
-        if (passwordInput.validity.patternMismatch) {
-            passwordInput.setCustomValidity('La contraseña no debe contener espacios');
-        } else {
-            passwordInput.setCustomValidity('');
-        }
     });
 });
